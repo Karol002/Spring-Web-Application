d203523758ac9bb3ec002ed8a847d79ff3d92fc5
Karol002 <karol.wojcik.002@gmail.com>
2022-11-30 07:37:32 +0100

    Configure Http request

---
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -0,0 +1,38 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.controller.TaskNotFoundException;
+import com.crud.tasks.domain.Task;
+import com.crud.tasks.repository.TaskRepository;
+import lombok.RequiredArgsConstructor;
+import org.aspectj.apache.bcel.generic.TargetLostException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class DbService {
+    @Autowired
+    private TaskRepository repository;
+
+    public List<Task> getAllTasks() {
+        return repository.findAll();
+    }
+
+    public Task saveTask(final Task task) {
+        return repository.save(task);
+    }
+
+    public Task getTask(final Long id) throws TaskNotFoundException {
+        return repository.findById(id).orElseThrow(TaskNotFoundException::new);
+    }
+
+    public void deleteById(Long id) throws TaskNotFoundException {
+        try {repository.deleteById(id);}
+        catch (Exception e) {
+            throw  new TaskNotFoundException();
+        }
+    }
+}
