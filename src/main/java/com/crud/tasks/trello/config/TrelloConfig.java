aade886a9348ad3aa6473dd8ca24a6f1e4b91d3a
Karol002 <karol.wojcik.002@gmail.com>
2023-01-14 14:31:35 +0100

    Mogenius prepare

--- src/main/java/com/crud/tasks/trello/config/TrelloConfig.java
+++ src/main/java/com/crud/tasks/trello/config/TrelloConfig.java
@@ -9,10 +9,10 @@ import org.springframework.stereotype.Component;
 public class TrelloConfig {
     //@Value("${trello.api.endpoint.prod}")
     private String trelloApiEndpoint = "https://api.trello.com/1";
-    @Value("${trello.app.key}")
-    private String trelloAppKey;
-    @Value("${trello.app.token}")
-    private String trelloToken;
-    @Value("${trello.app.user}")
-    private String trelloUser;
+    //@Value("${trello.app.key}")
+    private String trelloAppKey = "1131e01ffbd08dd322562c344798e668";
+    //@Value("${trello.app.token}")
+    private String trelloToken = "ATTA6e37af2de3c0fb17aa3b2cf2e2e966504c3ffc8786fb43b5c37118dc9cf3e304E02AA3EA";
+    //@Value("${trello.app.user}")
+    private String trelloUser = "karolwojcik6";
 }
